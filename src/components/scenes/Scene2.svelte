f32c7656c5d5216dd001223fccdfcbb59d36d442
Dayo <dayonel@hotmail.es>
2023-12-23 14:28:30 +0100

    Remove moving-curl attempt

--- src/components/scenes/Scene2.svelte
+++ src/components/scenes/Scene2.svelte
@@ -2,9 +2,6 @@
     import * as THREE from "three";
     import { Vector3 } from "three";
     import { createEventDispatcher, onMount } from "svelte";
-    import Simulator from "../moving-curl/Simulator.svelte";
-    import Particles from "../moving-curl/Particles.svelte";
-    import { Settings } from "../moving-curl/Settings";
 
     export let models: any[] = [];
     export let renderer: THREE.WebGLRenderer;
@@ -33,42 +30,13 @@
     onMount(async () => await init());
 
     const init = async () => {
-        lights();
-
-        // renderer.compile(scene, camera); // TODO
+        renderer.compile(scene, camera);
 
         mounted = true;
 
         dispatch("mount", { scene });
 
         loop();
-    };
-
-    const lights = () => {
-        const mesh = new THREE.Object3D();
-        mesh.position.set(0, 500, 0);
-
-        var ambient = new THREE.AmbientLight(0x333333);
-        mesh.add(ambient);
-
-        pointLight = new THREE.PointLight(0xffffff, 1, 700);
-        pointLight.castShadow = true;
-        pointLight.shadow.camera.near = 10;
-        pointLight.shadow.camera.far = 700;
-        pointLight.shadow.bias = 0.1;
-        pointLight.shadow.mapSize.width = 4096;
-        pointLight.shadow.mapSize.height = 2048;
-        mesh.add(pointLight);
-
-        var directionalLight = new THREE.DirectionalLight(0xba8b8b, 0.5);
-        directionalLight.position.set(1, 1, 1);
-        mesh.add(directionalLight);
-
-        var directionalLight2 = new THREE.DirectionalLight(0x8bbab4, 0.3);
-        directionalLight2.position.set(1, 1, -1);
-        mesh.add(directionalLight2);
-
-        scene.add(mesh);
     };
 
     const tone = () => {
@@ -87,9 +55,6 @@
         requestAnimationFrame(loop);
 
         const time = clock.getDelta();
-
-        pointLight.shadowDarkness = shadowDarkness +=
-            (Settings.SHADOW_DARKNESS - shadowDarkness) * 0.1;
     };
 </script>
 
@@ -97,5 +62,3 @@
     on:resize={() => resize()}
     on:orientationchange={() => resize()}
 />
-
-<Particles {renderer} {scene} {camera}></Particles>
