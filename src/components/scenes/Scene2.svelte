43202f7898280fd4c624e7dab796343c5987a7d3
Dayo <dayonel@hotmail.es>
2023-12-23 15:34:24 +0100

    Idea of scene 2 with particles.

--- src/components/scenes/Scene2.svelte
+++ src/components/scenes/Scene2.svelte
@@ -1,4 +1,8 @@
 <script lang="ts">
+    // @ts-ignore
+    import { gsap } from "gsap/dist/gsap.js";
+    // @ts-ignore
+    import { ScrollTrigger } from "gsap/dist/ScrollTrigger.js";
     import * as THREE from "three";
     import { Vector3 } from "three";
     import { createEventDispatcher, onMount } from "svelte";
@@ -10,11 +14,17 @@
     $: enabled, loop();
     $: enabled, tone();
     $: enabled, resize();
-    // $: models, meshToParticles();
+    $: enabled, gsapAnimate();
+    $: models, loadModel();
 
     const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(45, 1, 10, 3000);
-    camera.position.set(300, 60, 300).normalize().multiplyScalar(1000);
+    const camera = new THREE.PerspectiveCamera(
+        70,
+        window.innerWidth / window.innerHeight,
+        0.001,
+        1000,
+    );
+    camera.position.set(0, 0, 1);
     scene.add(camera);
     scene.userData.camera = camera;
     scene.userData.scene = 2;
@@ -22,14 +32,16 @@
     const dispatch = createEventDispatcher();
 
     let mounted = false;
-    let pointsMaterial: any;
-    let clock = new THREE.Clock();
-    let pointLight: any;
-    let shadowDarkness = 0.45;
+    let progress = 1;
+    let mesh: any;
+    let material: any;
+    let time = 0;
 
     onMount(async () => await init());
 
     const init = async () => {
+        gsap.registerPlugin(ScrollTrigger);
+
         renderer.compile(scene, camera);
 
         mounted = true;
@@ -37,6 +49,57 @@
         dispatch("mount", { scene });
 
         loop();
+    };
+
+    const loadModel = () => {
+        if (!models || models.length < 5) return;
+
+        console.log("loadModel");
+
+        const model = models[4];
+
+        material = new THREE.ShaderMaterial({
+            extensions: {
+                derivatives: true,
+            },
+            side: THREE.DoubleSide,
+            uniforms: {
+                time: { value: 1.0 },
+                progress: { value: 0.0 },
+            },
+            transparent: true,
+            vertexShader:
+                document.getElementById("morph-vs")?.textContent ?? "",
+            fragmentShader:
+                document.getElementById("morph-fs")?.textContent ?? "",
+            depthTest: false,
+            depthWrite: false,
+        });
+
+        model.traverse((obj: any) => {
+            if (obj.isMesh) {
+                mesh = new THREE.Points(obj.geometry, material);
+                mesh.position.set(0, 0.25, 0);
+                scene.add(mesh);
+            }
+        });
+    };
+
+    const gsapAnimate = () => {
+        if (!enabled) return;
+
+        ScrollTrigger.create({
+            scroller: "#scrolling",
+            start: "top top",
+            end: "+=" + window.innerHeight,
+            scrub: true,
+            trigger: "#scene2",
+            onUpdate: (self: any) => {
+                // Update the progress variable during the animation
+                progress = 1 - self.progress;
+                console.log(progress);
+            },
+        });
     };
 
     const tone = () => {
@@ -54,7 +117,14 @@
 
         requestAnimationFrame(loop);
 
-        const time = clock.getDelta();
+        time += 0.0001;
+
+        material.uniforms.time.value = time;
+        material.uniforms.progress.value = progress;
+
+        if (mesh) {
+            mesh.rotation.y = time * 0.5;
+        }
     };
 </script>
 
